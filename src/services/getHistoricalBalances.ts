e2920a52a1218d97dfd386bd69cd300f9768ef22
Constanze Meyer <constanze.xyz@gmail.com>
2023-09-10 16:24:47 +0200

    add getBalance and getTransactions methods

--- src/services/getHistoricalBalances.ts
+++ src/services/getHistoricalBalances.ts
@@ -1,5 +1,44 @@
 // This is just an example, it is not expected from you to keep this function
 // signature or even the file name. Feel free to change whatever you like.
+interface Balance {
+  date: Date;
+  amount: number,
+  currency: string,
+}
+
+interface Transaction {
+  amount: number;
+  currency: string;
+  date: Date;
+  status: string;
+}
+
+async function getBalance() {
+  const response = await fetch('https://uh4goxppjc7stkg24d6fdma4t40wxtly.lambda-url.eu-central-1.on.aws/balances', {
+    method: 'GET',
+    headers: {
+      'x-api-key': 'b4a4552e-1eac-44ac-8fcc-91acca085b98-f94b74ce-aa17-48f5-83e2-8b8c30509c18'
+    },
+  });
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  return await response.json() as Balance;
+}
+
+async function getTransactions() {
+  const response = await fetch('https://uh4goxppjc7stkg24d6fdma4t40wxtly.lambda-url.eu-central-1.on.aws/transactions', {
+    method: 'GET',
+    headers: {
+      'x-api-key': 'b4a4552e-1eac-44ac-8fcc-91acca085b98-f94b74ce-aa17-48f5-83e2-8b8c30509c18'
+    },
+  });
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  return await response.json() as Transaction[];
+}
+
 export function getHistoricalBalance() {
-  return { hello: "world" };
+
 }
